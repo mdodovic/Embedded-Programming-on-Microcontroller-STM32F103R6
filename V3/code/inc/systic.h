32acf9e1b0b5fdff3d05ced078ac462cf714280a
mdodovic <matija.dodovic@gmail.com>
2021-10-25 21:32:24 +0200

    add timer register configuration

---
+++ V3/code/inc/systic.h
@@ -0,0 +1,11 @@
+#ifndef _SYSTIC_H_
+#define _SYSTIC_H_
+
+#define STK_CTRL (*((uint32_t*) (0xE000E010 + 0x00)))
+#define STK_LOAD (*((uint32_t*) (0xE000E010 + 0x04)))
+#define STK_VAL (*((uint32_t*) (0xE000E010 + 0x08)))
+#define STK_CALIB (*((uint32_t*) (0xE000E010 + 0x0C)))
+
+
+
+#endif //_SYSTIC_H_
--- V3/code/src/systick.c
+++ V3/code/src/systick.c
@@ -1,6 +1,16 @@
+#include "systic.h"
 
-void systic_callback(){
+void systick_init()
+{
+	 	STK_LOAD = 8000000 - 1;
+	 	STK_VAL = 0;
+	 	STK_CTRL |= 0x07;
+}
 
+
+void __attribute__((weak)) systic_callback()
+{
+	// Empty
 }
 
 void systick_handler()
