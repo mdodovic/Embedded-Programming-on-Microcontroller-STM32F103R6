567bbe4c35ea6c148af788117bf9fd741bdbf415
mdodovic <matija.dodovic@gmail.com>
2021-11-07 12:09:22 +0100

    add SystemControllBlock register map

---
+++ timer-and-interrupts/workspace/code/inc/scb.h
@@ -0,0 +1,33 @@
+#ifndef _SCB_H_
+#define _SCB_H_
+
+#include <stdint.h>
+
+typedef struct
+{
+	uint32_t ICSR;
+	uint32_t AIRCR;
+	uint32_t SCR;
+	uint32_t CCR;
+
+	uint32_t SHPR1;
+	uint32_t SHPR2;
+	uint32_t SHPR3;
+
+	uint32_t SHCRS;
+	uint32_t CFSR;
+	uint32_t HFSR;
+
+	uint32_t dummy_0x30_0x34[1];
+
+	uint32_t MMAR;
+	uint32_t BFAR;
+
+} SCB_Register_Map;
+
+#define SCB ((SCB_Register_Map*)(0xE000ED00))
+
+#endif //_SCB_H_
+
+
+
