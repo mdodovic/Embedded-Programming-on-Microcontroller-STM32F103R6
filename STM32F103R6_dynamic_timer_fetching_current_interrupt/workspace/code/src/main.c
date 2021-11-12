3555350a02ddc03a9560837cf1c82c5bfbfff891
mdodovic <matija.dodovic@gmail.com>
2021-11-12 17:53:49 +0100

    create test for lma to vma data transfer

---
+++ STM32F103R6_dynamic_timer_fetching_current_interrupt/workspace/code/src/main.c
@@ -0,0 +1,21 @@
+#include <stdint.h>
+
+#include "nvic.h"
+#include "scb.h"
+#include "systick.h"
+#include "utility.h"
+
+uint8_t const rodata[] = "VMA:FLASH, LMA:FLASH";
+uint8_t data[] = "VMA:SRAM, LMA:FLASH";
+uint8_t bss;
+
+int main()
+{
+	bss = 0;
+
+	while(1)
+	{
+
+	}
+	return 0;
+}
