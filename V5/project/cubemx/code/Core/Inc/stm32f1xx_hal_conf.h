ce11e1fe8aaa3ae8b40f41ab92b1ec7b53af7a01
mdodovic <matija.dodovic@gmail.com>
2021-12-02 22:05:43 +0100

    change HSI to HSE and set its frequency to 4MHz

--- V5/project/cubemx/code/Core/Inc/stm32f1xx_hal_conf.h
+++ V5/project/cubemx/code/Core/Inc/stm32f1xx_hal_conf.h
@@ -44,7 +44,7 @@
 /*#define HAL_DMA_MODULE_ENABLED   */
 /*#define HAL_ETH_MODULE_ENABLED   */
 /*#define HAL_FLASH_MODULE_ENABLED   */
-/*#define HAL_GPIO_MODULE_ENABLED   */
+#define HAL_GPIO_MODULE_ENABLED
 /*#define HAL_I2C_MODULE_ENABLED   */
 /*#define HAL_I2S_MODULE_ENABLED   */
 /*#define HAL_IRDA_MODULE_ENABLED   */
@@ -83,7 +83,7 @@
   *        (when HSE is used as system clock source, directly or through the PLL).
   */
 #if !defined  (HSE_VALUE)
-  #define HSE_VALUE    8000000U /*!< Value of the External oscillator in Hz */
+  #define HSE_VALUE    4000000U /*!< Value of the External oscillator in Hz */
 #endif /* HSE_VALUE */
 
 #if !defined  (HSE_STARTUP_TIMEOUT)
