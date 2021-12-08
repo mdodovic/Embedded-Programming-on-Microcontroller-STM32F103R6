813980a2a7e61c52422719b706e61adf6069a3a2
mdodovic <matija.dodovic@gmail.com>
2021-12-08 09:16:53 +0100

    change Timebase Source for System Clock (from SysTick to TIM1)

--- STM32F103R6_configuration_timer_interrupts_CUBEMX/cubemx/code/Core/Src/main.c
+++ STM32F103R6_configuration_timer_interrupts_CUBEMX/cubemx/code/Core/Src/main.c
@@ -99,6 +99,8 @@ int main(void) {
 
 	NVIC->ISPR[0] |= 0b111;
 
+	SysTick_Config(ticks);
+
 	/* USER CODE END 2 */
 
 	/* Infinite loop */
@@ -146,6 +148,26 @@ void SystemClock_Config(void) {
 /* USER CODE BEGIN 4 */
 
 /* USER CODE END 4 */
+
+/**
+ * @brief  Period elapsed callback in non blocking mode
+ * @note   This function is called  when TIM1 interrupt took place, inside
+ * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+ * a global variable "uwTick" used as application time base.
+ * @param  htim : TIM handle
+ * @retval None
+ */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+	/* USER CODE BEGIN Callback 0 */
+
+	/* USER CODE END Callback 0 */
+	if (htim->Instance == TIM1) {
+		HAL_IncTick();
+	}
+	/* USER CODE BEGIN Callback 1 */
+
+	/* USER CODE END Callback 1 */
+}
 
 /**
  * @brief  This function is executed in case of error occurrence.
