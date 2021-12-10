e72b24dbe108083aa154787e7f5e95a67f0f9e02
mdodovic <matija.dodovic@gmail.com>
2021-12-10 11:16:13 +0100

    add switching between display information showing

--- STM32F103R6_stopwatch_tennis_game/cubemx/code/Core/Src/stopwatch.c
+++ STM32F103R6_stopwatch_tennis_game/cubemx/code/Core/Src/stopwatch.c
@@ -36,11 +36,13 @@
  * |  7  |||  1  |  0  |  0  |  0  |  1  |  1  |  1  |  1  ||| 8F  |
  * |  8  |||  1  |  0  |  0  |  0  |  0  |  0  |  0  |  0  ||| 80  |
  * |  9  |||  1  |  0  |  0  |  0  |  0  |  1  |  0  |  0  ||| 84  |
+ * |  d  |||  1  |  1  |  0  |  0  |  0  |  0  |  1  |  0  ||| C2  |
+ * |  A  |||  1  |  0  |  0  |  0  |  1  |  0  |  0  |  0  ||| 88  |
  * |-----|||-----|-----|-----|-----|-----|-----|-----|-----|||-----|
  *
  */
 uint8_t seven_segment_digits[] =
-{ 0x81, 0xCF, 0x92, 0x86, 0xCC, 0xA4, 0xA0, 0x8F, 0x80, 0x84 };
+{ 0x81, 0xCF, 0x92, 0x86, 0xCC, 0xA4, 0xA0, 0x8F, 0x80, 0x84, 0xC2, 0x88};
 
 extern TIM_HandleTypeDef htim1;
 
@@ -61,6 +63,44 @@ uint32_t minutes = 0;
 
 uint32_t stopwatch_paused = 1;
 
+#define STOPWATCH 0
+#define POINTS_IN_GAM 1
+#define POINTS_IN_SET 2
+
+uint32_t mode_of_display = STOPWATCH;
+
+void fill_digits_to_be_shown()
+{
+	switch (mode_of_display) {
+		case STOPWATCH:
+
+			digits_to_be_shown[0] = minutes / 10;
+			digits_to_be_shown[1] = minutes % 10;
+			digits_to_be_shown[2] = seconds / 10;
+			digits_to_be_shown[3] = seconds % 10;
+
+			break;
+		case POINTS_IN_GAM:
+
+			digits_to_be_shown[0] = 10;
+			digits_to_be_shown[1] = 10;
+			digits_to_be_shown[2] = 10;
+			digits_to_be_shown[3] = 10;
+
+			break;
+		case POINTS_IN_SET:
+
+			digits_to_be_shown[0] = 11;
+			digits_to_be_shown[1] = 11;
+			digits_to_be_shown[2] = 11;
+			digits_to_be_shown[3] = 11;
+
+			break;
+		default:
+			break;
+	}
+}
+
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
 	if(htim1.Instance == htim->Instance)
@@ -79,13 +119,11 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 					}
 				}
 
-				digits_to_be_shown[0] = minutes / 10;
-				digits_to_be_shown[1] = minutes % 10;
-				digits_to_be_shown[2] = seconds / 10;
-				digits_to_be_shown[3] = seconds % 10;
-
 			}
 		}
+
+		fill_digits_to_be_shown();
+
 		current_field_on_display = (current_field_on_display + 1) % 4;
 
 		GPIOC->ODR &= ~0xFFF;
@@ -97,14 +135,31 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 }
 
 
+
+
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
 
-	//if(GPIO_Pin == GPIO_PIN_11)
-	//{
+	if(GPIO_Pin == GPIO_PIN_8)
+	{
+		mode_of_display = (mode_of_display + 1) % 3;
+	}
+	if(GPIO_Pin == GPIO_PIN_9)
+	{
 		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_12);
 		stopwatch_paused = 1 - stopwatch_paused;
-	//}
+	}
+	if(GPIO_Pin == GPIO_PIN_10)
+	{
+		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_12);
+		stopwatch_paused = 1 - stopwatch_paused;
+	}
+	if(GPIO_Pin == GPIO_PIN_11)
+	{
+		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_12);
+		stopwatch_paused = 1 - stopwatch_paused;
+	}
+
 
 }
 
