fd319d40cb97b94f464768f0c7358ebaf5f915d7
mdodovic <matija.dodovic@gmail.com>
2022-01-24 20:14:14 +0100

    deffered interrupt handling with deamon task

--- V7/project/cubemx/code/Core/Src/example.c
+++ V7/project/cubemx/code/Core/Src/example.c
@@ -10,7 +10,7 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
-
+#include "timers.h"
 
 void exampleTaskFunction(void* parameters);
 void taskController(void* parameters);
@@ -21,13 +21,36 @@ TaskHandle_t controllerHandle;
 TaskHandle_t togglerHandle;
 TaskHandle_t defferedHandle;
 
+void callbackDeffered(void* p1, uint32_t p2);
+
 
 void exampleInit()
 {
 	//xTaskCreate(taskController, "controller", 128, NULL, 2, &controllerHandle);
 	//xTaskCreate(taskToggler, "toggler", 128, NULL, 2, &togglerHandle);
-	xTaskCreate(defferedTask, "defferedTask", 128, NULL, 2, &defferedHandle);
+	//xTaskCreate(defferedTask, "defferedTask", 128, NULL, 2, &defferedHandle);
+
+
 }
+
+
+void callbackDeffered(void* p1, uint32_t p2)
+{
+	HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_0);
+}
+
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+	if(GPIO_Pin == GPIO_PIN_0)
+	{
+		BaseType_t woken = pdFALSE;
+		xTimerPendFunctionCallFromISR(callbackDeffered, NULL, 0, &woken);
+//		vTaskNotifyGiveFromISR(defferedHandle, &woken);
+		portYIELD_FROM_ISR(woken);
+	}
+}
+
 
 void defferedTask(void* parameters)
 {
@@ -38,15 +61,6 @@ void defferedTask(void* parameters)
 	}
 }
 
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
-{
-	if(GPIO_Pin == GPIO_PIN_0)
-	{
-		BaseType_t woken = pdFALSE;
-		vTaskNotifyGiveFromISR(defferedHandle, &woken);
-		portYIELD_FROM_ISR(woken);
-	}
-}
 
 
 void taskController(void* parameters)
