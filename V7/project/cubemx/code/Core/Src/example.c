1e57766900cd8265440326778b40f484d8132edf
mdodovic <matija.dodovic@gmail.com>
2022-01-24 22:48:15 +0100

    queue buffering diode turn on by button

--- V7/project/cubemx/code/Core/Src/example.c
+++ V7/project/cubemx/code/Core/Src/example.c
@@ -11,88 +11,37 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "timers.h"
+#include "queue.h"
 
-void exampleTaskFunction(void* parameters);
-void taskController(void* parameters);
-void taskToggler(void* parameters);
-void defferedTask(void* parameters);
+QueueHandle_t queueHandle;
 
-TaskHandle_t controllerHandle;
-TaskHandle_t togglerHandle;
-TaskHandle_t defferedHandle;
-
-void callbackDeffered(void* p1, uint32_t p2);
-
+void taskToggled(void* parameters);
 
 void exampleInit()
 {
-	//xTaskCreate(taskController, "controller", 128, NULL, 2, &controllerHandle);
-	//xTaskCreate(taskToggler, "toggler", 128, NULL, 2, &togglerHandle);
-	//xTaskCreate(defferedTask, "defferedTask", 128, NULL, 2, &defferedHandle);
-
-
+	queueHandle = xQueueCreate(10, sizeof(uint32_t));
+	xTaskCreate(taskToggled, "taskToggler", 128, NULL, 2, NULL);
 }
 
-
-void callbackDeffered(void* p1, uint32_t p2)
+void taskToggled(void* parameters)
 {
-	HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_0);
+	uint32_t bufferForPinIndex;
+
+	while(1)
+	{
+		xQueueReceive(queueHandle, &bufferForPinIndex, portMAX_DELAY);
+		HAL_GPIO_TogglePin(GPIOC, bufferForPinIndex);
+	}
 }
 
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
-	if(GPIO_Pin == GPIO_PIN_0)
-	{
-		BaseType_t woken = pdFALSE;
-		xTimerPendFunctionCallFromISR(callbackDeffered, NULL, 0, &woken);
-//		vTaskNotifyGiveFromISR(defferedHandle, &woken);
-		portYIELD_FROM_ISR(woken);
-	}
-}
+	uint32_t itemToQueue = GPIO_Pin;
+	BaseType_t woken = pdFALSE;
 
+	xQueueSendToBackFromISR(queueHandle, &itemToQueue, &woken);
 
-void defferedTask(void* parameters)
-{
-	while(1)
-	{
-		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_0);
-	}
-}
-
-
-
-void taskController(void* parameters)
-{
-	while(1)
-	{
-		xTaskNotifyGive(togglerHandle);
-		vTaskDelay(pdMS_TO_TICKS(1000));
-	}
+	portYIELD_FROM_ISR(woken);
 
 }
-
-void taskToggler(void* parameters)
-{
-	while(1)
-	{
-		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_0);
-	}
-}
-
-
-void exampleTaskFunction(void* parameters)
-{
-	while(1)
-	{
-		HAL_GPIO_TogglePin(GPIOC, (uint32_t) parameters);
-//		vTaskDelay(1000);
-
-		for(int i = 0; i < 1000000; i++);
-	}
-}
-
-
-
