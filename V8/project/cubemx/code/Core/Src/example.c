8984a9cb30c41a8ec155c80c13cd515e9331e701
mdodovic <matija.dodovic@gmail.com>
2022-01-25 18:03:27 +0100

    add custom character

--- V8/project/cubemx/code/Core/Src/example.c
+++ V8/project/cubemx/code/Core/Src/example.c
@@ -43,11 +43,27 @@ static void exampleTask(void *parameters)
 
 	LCD_CommandEnqueue(LCD_INSTRUCTION, LCD_SET_DD_RAM_ADDRESS_INSTRUCTION | 0x03);
 
-	for(uint32_t i = 0; i < 10; i++)
-	{
-		LCD_CommandEnqueue(LCD_INSTRUCTION, LCD_SHIFT_INSTRUCTION | LCD_SHIFT_DISPLAY | LCD_SHIFT_DIRECTION_RIGHT);
-		vTaskDelay(pdMS_TO_TICKS(500));
-	}
+//	for(uint32_t i = 0; i < 10; i++)
+//	{
+//		LCD_CommandEnqueue(LCD_INSTRUCTION, LCD_SHIFT_INSTRUCTION | LCD_SHIFT_DISPLAY | LCD_SHIFT_DIRECTION_RIGHT);
+//		vTaskDelay(pdMS_TO_TICKS(500));
+//	}
+
+	LCD_CommandEnqueue(LCD_INSTRUCTION, LCD_SET_CG_RAM_ADDRESS_INSTRUCTION | 0x08);
+
+	LCD_CommandEnqueue(LCD_DATA, 0x04); //		_ _ X _ _
+	LCD_CommandEnqueue(LCD_DATA, 0x02); //		_ _ _ X _
+	LCD_CommandEnqueue(LCD_DATA, 0x11); //		X _ _ _ X
+	LCD_CommandEnqueue(LCD_DATA, 0x01); //		_ _ _ _ X
+	LCD_CommandEnqueue(LCD_DATA, 0x11); //		X _ _ _ X
+	LCD_CommandEnqueue(LCD_DATA, 0x02); //		_ _ _ X _
+	LCD_CommandEnqueue(LCD_DATA, 0x04); //		_ _ X _ _
+
+	LCD_CommandEnqueue(LCD_INSTRUCTION,
+	LCD_SET_DD_RAM_ADDRESS_INSTRUCTION | 0x48);
+
+	LCD_CommandEnqueue(LCD_DATA, 0x01);
+
 
 	while(1)
 	{
