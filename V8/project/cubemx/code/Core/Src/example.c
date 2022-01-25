f3a894fe0205e75d7eee70ce1846241db8630f0b
mdodovic <matija.dodovic@gmail.com>
2022-01-25 17:31:54 +0100

    create output

--- V8/project/cubemx/code/Core/Src/example.c
+++ V8/project/cubemx/code/Core/Src/example.c
@@ -8,11 +8,32 @@
 
 #include "example.h"
 
+#include "FreeRTOS.h"
+#include "task.h"
+
 #include "lcd.h"
+
+static void exampleTask(void *parameters)
+{
+	char line1[16] = "Mikroprocesorski";
+
+	LCD_CommandEnqueue(LCD_INSTRUCTION, LCD_SET_DD_RAM_ADDRESS_INSTRUCTION | 0x00);
+
+	for(uint32_t i = 0; i < 16; i++)
+	{
+		LCD_CommandEnqueue(LCD_DATA, line1[i]);
+		vTaskDelay(pdMS_TO_TICKS(100));
+	}
+
+	while(1)
+	{
+
+	}
+}
 
 void exampleInit()
 {
 	LCD_Init();
-
+	xTaskCreate(exampleTask, "exampleTask", 128, NULL, 5, NULL);
 }
 
