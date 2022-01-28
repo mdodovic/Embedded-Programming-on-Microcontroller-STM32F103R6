dc1765ea839d6d5567bfaee95e67f95312f66ff3
mdodovic <matija.dodovic@gmail.com>
2022-01-28 23:08:27 +0100

    turn (periodically) on diodes depending on speed

--- double_MCU_input_output_velocity_speed_state_singaling/cubemx2/code/Core/Src/mcu2.c
+++ double_MCU_input_output_velocity_speed_state_singaling/cubemx2/code/Core/Src/mcu2.c
@@ -15,9 +15,14 @@
 #include "lcd_driver.h"
 #include "usart_driver.h"
 
+#include "gpio.h"
+#include "timers.h"
+
+TimerHandle_t MCU2_TimerTask;
 
 void MCU_Task(void* p)
 {
+
 	LCD_CommandEnqueue(LCD_INSTRUCTION, LCD_SET_CG_RAM_ADDRESS_INSTRUCTION | 0x08);
 
 	LCD_CommandEnqueue(LCD_DATA, 0x1F); // XXXXX
@@ -59,16 +64,61 @@ void MCU_Task(void* p)
 				LCD_CommandEnqueue(LCD_DATA, ' ');
 			}
 		}
+		uint32_t v1 = htim3.Instance->CCR1;
+		uint32_t v2 = htim3.Instance->CCR2;
 
+
+		if(v1 + v2 == 0)
+		{
+			xTimerStart(MCU2_TimerTask, portMAX_DELAY);
+		}
+		else if(1 <= v1 + v2 && v1 + v2 <= 3)
+		{
+			// red
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+		}
+		else if(4 <= v1 + v2 && v1 + v2 <= 7)
+		{
+			// yellow
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+		}
+		else if(8 <= v1 + v2 && v1 + v2 <= 10)
+		{
+			// blue
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
+		}
+
+
+	}
+
+}
+
+void MCU2_TimerCallback(TimerHandle_t xTimer)
+{
+	HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_2);
+	uint32_t v1 = htim3.Instance->CCR1;
+	uint32_t v2 = htim3.Instance->CCR2;
+	if(v1 + v2 != 0)
+	{
+		xTimerStop(xTimer, 0);
 	}
 
 }
 
 void MCU2_Init()
 {
+//	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
 
 	LCD_Init();
 	UART_Init();
+
+	MCU2_TimerTask = xTimerCreate("tim", pdMS_TO_TICKS(500), pdTRUE, NULL, MCU2_TimerCallback);
 
 	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
 	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
